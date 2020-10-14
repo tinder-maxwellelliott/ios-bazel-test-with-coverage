679752c029484a77b69c9dcff8246cd3070e9da9
Maxwell Elliott <56700854+tinder-maxwellelliott@users.noreply.github.com>
2020-10-13 21:21:04 -0700

    Update lcov formatter

--- format_lcov.rb
+++ format_lcov.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 
 lcov_file_data = File.open("./CoverageData/info.lcov").read
-lcov_file_data.gsub!(/SF:.+__main__\//, "SF: ")
+lcov_file_data.gsub!(/SF:.+__main__\/Source/, "SF: Source")
 File.write("./CoverageData/info.lcov", lcov_file_data)
